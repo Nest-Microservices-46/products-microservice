82e81d8776970752f888c33c529399debe97b3d8
Jeremias Muriette <jnahuel46@gmail.com>
2026-07-24 19:42:56 -0300

    starting docker builder

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.TCP,
+    transport: Transport.NATS,
     options: {
-      port: 3000,
+      servers: envs.natsServers,
     }
   });
 
